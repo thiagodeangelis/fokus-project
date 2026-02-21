2fec6aeacf60999802a4cefce864ba88698844f6
Thiago De Angelis <thiagodevangelis@gmail.com>
2026-02-21 08:29:53 -0300

    audios de play, pause e tempo finalizado inseridos

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const textoStrong = document.querySelector('.app__title-strong');
 const botoes = document.querySelectorAll('.app__card-button');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio ('/sons/luna-rise-part-one.mp3');
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 const timerFoco = 1500;
 const timerDescansoCurto = 300;
 const timerDescansoLongo = 900;
@@ -86,8 +89,9 @@ function alterarContexto(contexto) {
 
     const contagemRegressiva = () => {
         if(tempoDecorridoEmSegundos <= 0){
-            zerar()
+        audioTempoFinalizado.play()
          alert('Tempo Finalizado!')
+         zerar()
          return
         }
         tempoDecorridoEmSegundos -= 1;
@@ -99,10 +103,11 @@ function alterarContexto(contexto) {
     function iniciarOuPausar () {
 
         if(intervaloId){
-
+           audioPausa.play()
             zerar()
             return
         }
+         audioPlay.play();
         intervaloId = setInterval(contagemRegressiva, 1000)
     }
 
